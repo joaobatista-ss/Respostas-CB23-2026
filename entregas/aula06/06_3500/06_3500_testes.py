4ad2250081273219c0b99c828e37c97e1b716e5e
João Batista (3500) <al.joao.batista@impatech.edu.br>
2026-09-03 16:09:25 -0300

    adiciona primeira estrutura para testes de PilhaEncadeada

---
+++ entregas/aula06/06_3500/06_3500_testes.py
@@ -0,0 +1,23 @@
+from P06_3500_fila_encadeada import PilhaEncadeada
+from unittest import TestCase
+
+class TestPilhaEncadeada(TestCase):
+    """
+    Testes obrigatórios (unittest) para a pilha:
+        Ordem LIFO em sequência de push/pop;
+        Pop e topo em pilha vazia;
+        Coerência de len após inserções e remoções;
+        Alternância de operações;
+        Armazenamento de itens de tipos diferentes, incluindo valores repetidos e None.
+    """
+    def test_lifo(self):
+        pass
+
+    def test_len(self):
+        pass
+
+    def test_alternating_operations(self):
+        pass
+
+    def test_storage_of_different_types(self):
+        pass
