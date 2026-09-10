affa726c094a8fc06dcc0acf4a50c85e3a9c968e
João Batista (3500) <al.joao.batista@impatech.edu.br>
2026-09-10 17:36:00 -0300

    Testes: implementa testes de FilaEncadeada

--- entregas/aula06/06_3500/06_3500_testes.py
+++ entregas/aula06/06_3500/06_3500_testes.py
@@ -1,6 +1,9 @@
-from P06_3500_fila_encadeada import PilhaEncadeada
-from unittest import TestCase, main
 import random
+from unittest import TestCase, main
+
+from P06_3500_fila_encadeada import FilaEncadeada
+from P06_3500_pilha_encadeada import PilhaEncadeada
+
 
 class TestPilhaEncadeada(TestCase):
     """
@@ -117,7 +120,120 @@ class TestPilhaEncadeada(TestCase):
                 size -= 1
                 self.assertEqual(stack.pop(), elem)
                 self.assertEqual(len(stack), size)
-            
+
+
+class TestFilaEncadeada(TestCase):
+    """
+    Testes obrigatórios (unittest) para a fila:
+        ordem FIFO;
+        intercalação de enfileirar e desenfileirar;
+        esvaziar e voltar a usar a mesma instância;
+        desenfileirar e frente em fila vazia; 
+        coerência de len.
+    """
+
+    def test_fifo(self):
+        """
+        Teste de ordem FIFO em sequencia de enfileirar/desenfileirar.
+
+        Adiciona três elementos em uma FilaEncadeada vazia, depois remove os elementos.
+        Verifica as operações frente, enfileirar, desenfileirar.
+        """
+        queue = FilaEncadeada()
+        queue.enfileirar("A")
+        queue.enfileirar("B")
+        queue.enfileirar("C")
+
+        self.assertEqual(queue.frente(), 'A')
+        self.assertEqual(queue.desenfileirar(), 'A')
+
+        self.assertEqual(queue.frente(), 'B')
+        self.assertEqual(queue.desenfileirar(), 'B')
+
+        self.assertEqual(queue.frente(), 'C')
+        self.assertEqual(queue.desenfileirar(), 'C')
+
+    def test_alternating_operations(self):
+        """
+        Teste de alternância de operações.
+
+        Adiciona ou remove elementos em uma fila de maneira imprevisível, fazendo verificações dos métodos:
+            enfileirar, desenfileirar, frente, len.
+        """
+        queue = FilaEncadeada()
+        added_queue = 0
+        removed_queue = 0
+
+        for _ in range(10000):
+            operation = random.choice(["enfileirar", "desenfileirar", "frente", "len"])
+
+            if operation == "enfileirar":
+                added_queue += 1
+                queue.enfileirar(str(added_queue))
+                continue
+
+            if operation == "desenfileirar":
+                if not added_queue-removed_queue:
+                    continue
+                self.assertEqual(queue.desenfileirar(), str(removed_queue+1))
+                removed_queue += 1
+                continue
+
+            if operation == "frente":
+                if not added_queue-removed_queue:
+                    continue
+                self.assertEqual(queue.frente(), str(removed_queue+1))
+                continue
+
+            if operation == "len":
+                self.assertEqual(len(queue), added_queue-removed_queue)
+
+    def test_empty_and_reuse_the_same_instance(self):
+        """
+        Teste esvaziar e voltar a usar a mesma instância
+
+        Adiciona e remove todos os elementos diversas vezes.
+        """
+
+        queue = FilaEncadeada()
+        for _ in range(40):
+            for _ in range(100):
+                queue.enfileirar(1)
+            for i in range(100):
+                self.assertEqual(len(queue), 100-i)
+                self.assertEqual(queue.desenfileirar(), 1)
+
+    def test_dequeue_and_peek_in_empty_queue(self):
+        """
+        Teste de desenfileirar e frente em FilaEncadeada vazia.
+
+        Verifica se a fila encadeada retorna corretamente exceção IndexError durante
+        as operações desenfileirar e frente com a fila vazia.
+        """
+        queue = FilaEncadeada()
+        self.assertRaises(IndexError, queue.desenfileirar)
+        self.assertRaises(IndexError, queue.frente)
+
+    def test_len(self):
+        """
+        Teste de coerência de len após inserções e remoções.
+
+        Adiciona 1000 elementos e os remove, enquanto faz verificações de __len__.
+        """
+
+        queue = FilaEncadeada()
+
+        # Testes adicionando elementos
+        for c in range(1, 1001):
+            queue.enfileirar(1)
+            self.assertEqual(len(queue), c)
+
+        # Testes removendo elementos
+        for c in range(1, 1001):
+            queue.desenfileirar()
+            self.assertEqual(len(queue), 1000-c)
+
 
 if __name__ == '__main__':
-    main(verbosity=2)
+    # main(verbosity=2)
+    main()
